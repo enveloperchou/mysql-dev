3d22f8dffcb6c3531d87e34225bf100646f9ebb1
enveloperchou <enveloperchou@gmail.com>
2016-12-14 10:34:02 +0800

    alter table schema

--- create_blog.sql
+++ create_blog.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE blog;
-
-CREATE TABLE content(
+USE blog;
+CREATE TABLE blog(
 	content MEDIUMTEXT not null,
 	title VARCHAR(256) not null,
-	time TIMESTAMP not null,
+	description VARCHAR(256),
+	subtitle VARCHAR(256) not null,
+	time INT(11) not null,
 	PRIMARY KEY(title, time)
 );
